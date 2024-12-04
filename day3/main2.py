85abe04eb34e772d43d0889277cbd8e7d374aefa
Simon Merrick <simon.merrick@trademe.co.nz>
2024-12-04 13:36:57 +1300

    Day3

---
+++ day3/main2.py
@@ -0,0 +1,35 @@
+import re
+
+# Define the pattern to search for
+pattern = r'mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don\'t\(\)'
+
+# Path to the file you want to search
+file_path = 'input.txt'
+
+# Open the file and read its contents
+with open(file_path, 'r') as file:
+    file_contents = file.read()
+
+# Perform a global search (find all matches) in the file contents
+matches = re.findall(pattern, file_contents)
+
+# Check if any matches were found
+do = True
+if matches:
+    print("Found matches:")
+    total = 0
+    for match in matches:
+        print(match)
+        if match == "do()":
+            do = True
+        elif match == "don't()":
+            do = False
+        elif do is True:
+            f = [int(x) for x in match[4:-1].split(",")]
+            subtotal = f[0] * f[1]
+            print(f"{f[0]} * {f[1]} = {subtotal}")
+            print(f"{total} + {subtotal} = {total+subtotal}")
+            total += subtotal
+    print(total)
+else:
+    print("No matches found.")
